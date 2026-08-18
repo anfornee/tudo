da6da71c6cbf1815dabd7d44aed6376b0a36b599
Anthony Pereira <contact@theanthonypereira.com>
2026-08-18 19:47:52 -0400

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
       <h1>hello there...</h1>
+      <Button onClick={() => console.log('clicked')}>Click me</Button>
     </main>
   );
 }
