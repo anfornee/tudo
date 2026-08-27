00cdc75f52f4878c438883f4cf90fbfee821d011
Anthony Pereira <contact@theanthonypereira.com>
2026-08-27 11:20:02 -0400

    the best sudoku

--- components/sudoku/SudokuBoard.tsx
+++ components/sudoku/SudokuBoard.tsx
@@ -59,9 +59,7 @@ export function SudokuBoard({
         const isMatching = selectedValue !== 0 && value === selectedValue;
 
         const isIncorrect =
-          !isGiven &&
-          value !== 0 &&
-          "font-semibold text-primary brightness-125";
+          !isGiven && value !== 0 && value !== solution[index];
 
         const hasRightBoxBorder = column === 2 || column === 5;
 
